4571f44d2bdfa342d5b1abadfcd6f3658627572e
RodMan <rod@littlelives.com>
2017-11-09 23:43:21 +1030

    Update footer-no-sidebar.php
    I removed line 11. It seems it shouldn't be there.

--- footer-no-sidebar.php
+++ footer-no-sidebar.php
@@ -8,7 +8,7 @@
     <!-- Note if lt IE9 support needed, use jQuery 1.x.x, v2 doesn't support it -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-2.0.3.min.js"%3E%3C/script%3E'))</script>
-    // Something
+    
     <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/general.js"></script>
 <!-- All other scripts -->
 
